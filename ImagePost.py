bc095c444b6ddabc218107d88220efbcc8effa02
idanlisha <idan.lisha@gmail.com>
2025-01-27 18:16:25 +0200

    Syalin

--- ImagePost.py
+++ ImagePost.py
@@ -1,12 +1,10 @@
 from post import *
 
-def image():
+def image(post):
     image1 = pygame.image.load('Images/putin.jpg')
     image1 = pygame.transform.scale(background, (POST_WIDTH, POST_HEIGHT))
-    """
     image2 = pygame.image.load('Images/noa_kirel.jpg')
-    image2 = pygame.transform.scale(background, (POST_WIDTH, POST_HEIGHT))
-    image3 = pygame.image.load('Images/noa_kirel.jpg')
-    image3 = pygame.transform.scale(background, (POST_WIDTH, POST_HEIGHT))
-"""
+    image2 = pygame.transform.scale(background, (POST_WIDTH, POST_HEIGHT)
+    image3 = pygame.image.load('Images/stalin.jpg')
+    image3 = pygame.transform.scale(background, (POST_WIDTH, POST_HEIGHT)
 
